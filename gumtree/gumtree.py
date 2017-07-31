596f9c2a9907405c97be3851f5dd3f26611ddc79
Jake Treacher <git@jaketreacher.com>
2017-07-31 17:11:41 +0800

    Add threading and fix up various things that were breaking

--- gumtree/gumtree.py
+++ gumtree/gumtree.py
@@ -1,8 +1,17 @@
 from bs4 import BeautifulSoup
+from multiprocessing.dummy import Pool
+import pickle
 import sys
 import re
 import requests
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
+
+_CHROME = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 ' \
+           '(KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
+
+# Used for threading to show progress
+count = 0
+total = 0
 
 class AdSoldException(Exception):
     def __init__(self, message):
@@ -13,40 +22,65 @@ class GSearch:
         item_urls = self.scrape_pages(url)
 
         self.items = []
-        print('Scraping items, max: %d' % len(item_urls))
-        for item_url in item_urls:
-            print(item_url)
-            self.items.append(GItem(item_url))
+        print('Scraping items, found: %d' % len(item_urls))
+        
+        global count, total
+        count = 1
+        total = len(item_urls)
+        with Pool(8) as pool:
+            self.items = pool.map(self.gitem_pool, item_urls)
+
+        self.errors = [item for item in self.items 
+            if not isinstance(item, GItem)]
+
+        self.items = [item for item in self.items 
+            if isinstance(item, GItem)]
+
+    def gitem_pool(self, url):
+        print_progress(url)
+        try:
+            return GItem(url)
+        except Exception as e:
+            return (url, e)
 
     def scrape_pages(self, url):
         """Scrape the urls in each page for items
         """
         page_urls = self.get_page_urls(url)
 
-        item_urls = []
-        print('Scraping pages, max: %d' % len(page_urls))
-        for page_url in page_urls:
-            print(page_url)
-            result = requests.get(page_url)
-            soup = BeautifulSoup(result.content, 'html.parser')
+        print('Scraping pages, found: %d' % len(page_urls))
+        global count, total
+        count = 1
+        total = len(page_urls)
+        with Pool(8) as pool:
+            item_urls = pool.map(self.scrape_page_pool, page_urls)
 
-            listings = soup.find_all('a',
-                                    {'class':'ad-listing__title-link',
-                                     'itemprop':'url'},
-                                     href=True)
+        #flatten list
+        item_urls = [item for sub_list in item_urls for item in sub_list]
 
-            for listing in listings:
-                item_urls.append(listing['href'])
-
+        #convert to absolute url
         item_urls = [urljoin(url, item) for item in item_urls]
 
         return item_urls
 
 
     def scrape_page_pool(self, url):
-        """To be used for multithreading
+        """To be used for threading
         """
-        pass
+        print_progress(url)
+        item_urls = []
+        result = requests.get(url, headers=_CHROME)
+        soup = BeautifulSoup(result.content, 'html.parser')
+
+        listings = soup.find_all('a',
+                                {'class':'ad-listing__title-link',
+                                 'itemprop':'url'},
+                                 href=True)
+
+        for listing in listings:
+            item_urls.append(listing['href'])
+
+        return item_urls
 
     def get_page_urls(self, url):
         """Checks the url in the 'last page' pagination button
@@ -56,14 +90,14 @@ class GSearch:
 
         Generates urls of the pages based on this 'max' number.
         """
-        result = requests.get(url)
+        result = requests.get(url, headers=_CHROME)
         result.raise_for_status()
 
         soup = BeautifulSoup(result.content, 'html.parser')
         paginator = soup.find('div', {'class':'paginator'})
         last_button = paginator.find('a',
             {'class':'paginator__button-last'}, 
-            href=True,)
+            href=True)
 
         max_page = 1
         if last_button:
@@ -80,7 +114,7 @@ class GSearch:
 
 class GItem:
     def __init__(self, url):
-        result = requests.get(url)
+        result = requests.get(url, headers=_CHROME)
         result.raise_for_status()
 
         soup = BeautifulSoup(result.content, 'html.parser')
@@ -105,8 +139,8 @@ class GItem:
 
     def parse_price(self, soup):
         ad_price = soup.find('div', {'id': 'ad-price'})
-        text = ad_price.text.strip()
         if ad_price:
+            text = ad_price.text.strip()
             result = re.search('\$([0-9]*\.[0-9]{2})', text)
             if result:
                 price = result.group(1)
@@ -114,10 +148,10 @@ class GItem:
                 price = None
 
             negotiable = 'Negotiable' in text
-        else:
-            raise AdSoldException('"%s" has been sold.' % self.title)
 
-        return price, negotiable
+            return price, negotiable
+        else:
+            return None, None
 
     def parse_images(self, soup):
         images = []
@@ -156,10 +190,17 @@ class GItem:
         if container:        
             sections = container.find_all('dl')
 
+            # Clear bogus sections with no content
+            sections = [sect for sect in sections if sect.text]
             for section in sections:
-                key = section.find('dt').text.strip() \
-                    .replace(':','').replace(' ', '_').lower()
-                value = section.find('dd').text.strip()
+                dt = section.find('dt')
+                key = dt.text.strip() \
+                    .replace(':','') \
+                    .replace(' ', '_') \
+                    .lower()
+
+                dd = section.find('dd')
+                value = dd.text.strip()
 
                 if key not in extras:
                     extras[key] = value
@@ -171,8 +212,13 @@ class GItem:
 
 class GUser:
     def __init__(self, profile):
-        href = profile.find('a', href=True)['href']
-        name, userid = re.match('/s-seller/(.*?)/(.*?)$', href).groups()
+        a_sect = profile.find('a', href=True)
+        result = re.match('/s-seller/(.*?)/(.*?)$', a_sect['href'])
+        if result:
+            name, userid = result.groups()
+        else:
+            name = a_sect.text.strip()
+            userid = a_sect['href']
 
         member_since = profile.find('span',
             {'class':'seller-profile__member-since'}).text
@@ -183,10 +229,58 @@ class GUser:
         self.userid = userid
         self.created = created
 
+def prepare_args(urls):
+    total = len(urls)
+    args = list(enumerate(urls))
+    args = [(url, idx+1, total) for idx, url in args]
+    return args
+
+def print_progress(url):
+    global count, total
+    url = limit(url)
+
+    # count += 1 on the same line for threading purposes
+    # if it is moved to a second line, multiple threads will print
+    #   the same number
+    print("{:60} | ({}/{})".format(url, count, total)); count += 1
+
+def limit(string, print_length=58):
+    """Limit the length of a string to print_length characters
+    Replaces the middle with ...
+    """
+    if len(string) > print_length:
+        trim = int((print_length-3)/2)
+        return string[:trim] + '...' + string[-trim:]
+    else:
+        return string
+
+def prepare_url(url):
+    url_p = urlparse(url)
+
+    # ensure page-1 appears in the url
+    path = url_p.path
+    result = re.search('(page-[0-9]*?)\/', path)
+    if result:
+        path = path.replace(result.group(1), 'page-1')
+    else:
+        # insert page-1 as second last path segment
+        path = path.split('/')
+        path.insert(-1, 'page-1')
+        path = '/'.join(path)
+    url_p = url_p._replace(path=path)
+
+    # add pageSize=96 query so it forces more results
+    query = url_p.query
+    query = query.split('&')
+    query.append('pageSize=96')
+    query = '&'.join(query)
+    url_p = url_p._replace(query=query)
+
+    return url_p.geturl()
+
 if __name__ == '__main__':
-    url = 'https://www.gumtree.com.au/s-belmont/page-1/l3008329?price-type=free'
-    try:
-        item = GSearch(url)
-        import pdb;pdb.set_trace()
-    except AdSoldException as e:
-        print(e.message)
+    url = sys.argv[1]
+    url = prepare_url(url)
+    gsearch = GSearch(url)
+    with open('result.pkl', 'wb') as file:
+        pickle.dump(gsearch, file)
